45117919c2a0ea7f5ce65d1806942b13467346fc
alberto gonzalez trastoy <albertogontras@gmail.com>
2020-04-04 12:53:39 -0500

    demo working

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-A browser action with a popup that changes the page color
-=======
+#Media tab capture chrome extension
+A chrome browser extension that records the current tab and let's you download or play it later.
 
-Change the current page color
+### API Explanation
 
-[Zipfile](http://developer.chrome.com/extensions/examples/api/browserAction/set_page_color.zip)
+1. First step is to initialize the constructor `new RecordRTC_Extension()`.
+2. Second step is, pass two parameters on `startRecording`. First paramter is named as `recording-formats` and last parameter is named as `recording-start-callback`.
+3. Manually stop the recording using `stopRecording` method. Callback contains two arguments. First argument is `Blob` object and second argument is `error` string.
 
-Content is licensed under the [Google BSD License](https://developers.google.com/open-source/licenses/bsd).
+###Credit where credit is due:
+Based on: [https://github.com/muaz-khan/Chrome-Extensions/tree/master/screen-recording](https://github.com/muaz-khan/Chrome-Extensions/tree/master/screen-recording)
 
-Calls
------
-
-* [tabs.executeScript](https://developer.chrome.com/extensions/tabs#method-executeScript)
+## License
+[MIT license](https://github.com/muaz-khan/Chrome-Extensions/blob/master/LICENSE)
--- popup.js
+++ popup.js
@@ -1,18 +1,42 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
+let runtimePort = chrome.runtime.connect({
+    name: location.href.replace(/\/|:|#|\?|\$|\^|%|\.|`|~|!|\+|@|\[|\||]|\|*. /g, '').split('\n').join('').split('\r').join('')
+});
+
+runtimePort.onMessage.addListener(function(message) {
+    if (!message) {
+        return;
+    }
+});
+
+function recordActionNotify(e) {
+    let action = e.target.id;
+    console.log(action);
+
+    chrome.storage.sync.set({
+        isRecording: 'false'
+    }, function() {
+        runtimePort.postMessage({
+            action: action
+        });
+        window.close();
+    });
+}
+
 function click(e) {
-    chrome.tabs.executeScript(null,
-        {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
-    window.close();
+    console.log(e);
+    // chrome.tabs.executeScript(null,
+    //     {code:"document.body.style.backgroundColor='green'"},
+    //     function (res) {
+    //         console.log(res);
+    //     });
+    recordActionNotify(e)
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    var divs = document.querySelectorAll('div');
-    for (var i = 0; i < divs.length; i++) {
+    let divs = document.querySelectorAll('div');
+    for (let i = 0; i < divs.length; i++) {
         divs[i].addEventListener('click', click);
     }
 });
